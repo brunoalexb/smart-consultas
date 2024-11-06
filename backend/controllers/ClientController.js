7d9ce0d876f0924ace972d69cba63726b85f6c49
brunoalexb <brunoale77@gmail.com>
2024-11-05 22:48:44 -0300

    novas alterações no banck e no gitignore

--- backend/controllers/ClientController.js
+++ backend/controllers/ClientController.js
@@ -1,62 +1,29 @@
 const Client = require('../models/client');
 
-// Função para criar um cliente
-exports.create = (req, res) => {
-  const { name, birthdate, phone, email, address, notes } = req.body;
-  Client.create({ name, birthdate, phone, email, address, notes })
-    .then(client => res.status(201).send(client))
-    .catch(error => res.status(400).send(error));
-};
 
+exports.create = (client) => {
+  const { name, birthdate, phone, email, address, notes } = client;
+  return Client.create({name, birthdate, phone, email, address, notes});
+}
 
 // Função para buscar todos os clientes
-exports.findAll = (req, res) => {
-  Client.findAll()
-    .then(clients => res.status(200).send(clients))
-    .catch(error => res.status(500).send({ message: "Erro ao buscar clientes", error }));
+exports.findAll = () => {
+  return Client.findAll();
 };
 
 // Função para buscar cliente por ID
-exports.findOne = (req, res) => {
-  const { id } = req.params;
-  Client.findByPk(id)
-    .then(client => {
-      if (!client) {
-        return res.status(404).send({ message: "Cliente não encontrado" });
-      }
-      res.status(200).send(client);
-    })
-    .catch(error => res.status(500).send({ message: "Erro ao buscar cliente", error }));
+exports.findOne = (id) =>{
+  return Client.findByPk(id);
 };
+
 
 // Função para atualizar cliente por ID
-exports.update = (req, res) => {
-  const { id } = req.params;
-  const { name, birthdate, phone, email, address, notes } = req.body;
-
-  Client.update(
-    { name, birthdate, phone, email, address, notes },
-    { where: { id } }
-  )
-    .then(([rowsUpdated]) => {
-      if (rowsUpdated === 0) {
-        return res.status(404).send({ message: "Cliente não encontrado" });
-      }
-      res.status(200).send({ message: "Cliente atualizado com sucesso" });
-    })
-    .catch(error => res.status(500).send({ message: "Erro ao atualizar cliente", error }));
+exports.update = (id, clientData) => {
+  return Client.update(clientData, {where: {id}});
 };
 
-// Função para deletar cliente por ID
-exports.delete = (req, res) => {
-  const { id } = req.params;
 
-  Client.destroy({ where: { id } })
-    .then(rowsDeleted => {
-      if (rowsDeleted === 0) {
-        return res.status(404).send({ message: "Cliente não encontrado" });
-      }
-      res.status(200).send({ message: "Cliente deletado com sucesso" });
-    })
-    .catch(error => res.status(500).send({ message: "Erro ao deletar cliente", error }));
+// Função para deletar cliente por ID
+exports.delete = (id) => {
+  return Client.destroy({ where: { id } });
 };
--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const router = express.Router();
-const clients = require('../controllers/ClientController');
+const clientController = require('../controllers/ClientController');
 
-router.post('/clients', clients.create);
-router.get('/clients', clients.findAll);
-router.get('/clients/:id', clients.findOne);
-router.put('/clients/:id', clients.update);
-router.delete('/clients/:id', clients.delete);
+
+router.post('/clients', async(req, res) => {
+    try{
+        const client = req.body;
+        const result = await clientController.create(client);
+        res.status(201).json(result);
+    } catch(error){
+        res.status(400).send(error)
+    }
+    
+});
+
+
+router.get('/clients', async(req, res) => {
+    try{
+        const clients = await clientController.findAll();
+        res.status(200).json(clients);
+    }catch (error){
+        res.status(500).send({message: "Erro ao buscar clientes", error});
+    }
+
+});
+
+
+router.get('/clients/:id', async(req, res) =>{
+    try{
+        const { id } = req.params;
+        const client = await clientController.findOne(id);
+        res.status(200).json(client);
+    }catch(error){
+        res.status(500).send({message: "Erro ao buscar cliente", error});
+    }
+});
+
+router.put('/clients/:id', async(req, res) =>{
+    try{
+        const { id } = req.params;
+        const clientData = req.body;
+        const result = await clientController.update(id, clientData)
+        res.status(200).json(result);
+    }catch(error){
+        res.status(500).send({message: "Erro ao atualizar cliente", error});
+    }
+    
+});
+
+
+router.delete('/clients/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+        const client = await clientController.delete(id);
+        res.status(200).json(client);
+    }catch(error){
+        res.status(500).send({message: "Erro ao deletar cliente", error});
+    }
+});
 
 module.exports = router;
